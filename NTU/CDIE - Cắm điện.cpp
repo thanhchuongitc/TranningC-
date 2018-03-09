57982396e7a0c0d018edaa21e1f8edaa05ba4d4f
Trần Thanh Chương <33802536+thanhchuongitc@users.noreply.github.com>
2018-03-09 10:32:43 +0700

    Update CDIE - Cắm điện.cpp

--- NTU/CDIE - Cắm điện.cpp
+++ NTU/CDIE - Cắm điện.cpp
@@ -1,44 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main ()
 {
-    
-    int n, m;
-    cin >> n >> m;
-    if (m==1)
-    {
-        cout << 0;
-        return 0;
-    }
-    vector<int> a;
-    int temp;
-    for (int i=0;i!=n;i++)
-    {
-        cin >> temp;
-        a.push_back(temp);
-    }
-    sort(a.begin(),a.end(),greater<int>());
-    queue<int> q;
-    for (int i=0;i!=a.size();i++)
-        q.push(a[i]);
-    int tong = q.front();
-    q.pop();
-    int dem=1;
-    while (!q.empty())
-    {
-        if (tong < m)
-        {
-            tong+= q.front() - 1;
-            dem++;
-            q.pop();
-        }
-        else
-        {
-            cout << dem;
-            return 0;
-        }
-    }
-    cout << -1;
-    return 0;
+	int n,m,x;
+	cin >> n >> m;
+	if (m==1)
+	{
+		cout << 0;
+		return 0;
+	}
+	vector<int> v;
+	for (int i=0;i<n;i++)
+	{
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(),v.end(),greater<int>());
+	int sum = v[0],count=1;
+	for (int i=1;i<v.size();i++)
+	{
+		if (sum>=m)
+		{
+			cout << count;
+			return 0;
+		}
+		sum+= v[i]-1;
+		count++;
+	}
+	cout << -1;
+	return 0;
 }
