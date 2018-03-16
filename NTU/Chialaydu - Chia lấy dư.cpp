34993974abadf6bece8890b0c90f5e76d9c6d780
Trần Thanh Chương <33802536+thanhchuongitc@users.noreply.github.com>
2018-03-16 11:28:12 +0700

    Create Chialaydu - Chia lấy dư.cpp

---
+++ NTU/Chialaydu - Chia lấy dư.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<int> v (10005,false);
+    int x;
+    int dem = 0;
+    while (n--)
+    {
+        cin >> x;
+        if (!v[x%k])
+        {
+            dem++;
+            v[x%k] = true;
+        }
+
+    }
+    cout << dem;
+    return 0;
+}
